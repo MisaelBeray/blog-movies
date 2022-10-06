4d70e0972cafa2b5352d08d8c1d7e39303aebee6
Misael Beray <misaelberay@gmail.com>
2022-10-06 16:35:45 -0300

    feat(article): added paginate

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,8 @@
 class Article < ApplicationRecord
     validates :title, presence: true, length: {minimum: 5}
-    validates :body, presence: true, length: {minimum:10, maximum:500}
+    validates :body, presence: true, length: {minimum:10}
+    paginates_per 2
+
+    scope :desc_order, -> { order(created_at: :desc) }
+    scope :without_highlights, -> (ids) { where("id NOT IN(#{ids})") }
 end
